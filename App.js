753d0525f0fa9d60f7253096a2dec6008d5a55df
joelbowen <joel.michael.bowen@gmail.com>
2017-11-29 22:16:35 -0500

    basic styled components usage

--- App.js
+++ App.js
@@ -1,23 +1,24 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import styled from 'styled-components/native'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
+const Container = styled.View`
+  align-items: center;
+  background-color: #fff;
+  flex: 1;
+  justify-content: center;
+`
+const Text = styled.Text`
+  line-height: 24;
+`
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <Container>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      </Container>
     )
   }
 }
